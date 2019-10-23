3eaddba36ef38a2b1954e8f658c352f922eea58c
MR Matthias <matthias.nyman@hotmail.com>
2019-10-23 16:23:47 +0200

    better styling and font

--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import './stopwatch.css'
-import ms from 'pretty-ms';
-import humanizeDuration from 'humanize-duration';
-import Watch from '../img/stopwatch.png';
-import Button from '../img/button.png';
+import "./stopwatch.css";
+import ms from "pretty-ms";
+import humanizeDuration from "humanize-duration";
+import Watch from "../img/stopwatch.png";
+import Button from "../img/button.png";
 
 class Stopwatch extends Component {
   constructor() {
@@ -24,101 +24,119 @@ class Stopwatch extends Component {
 
   startTimer() {
     if (!this.state.isOn) {
-
       this.setState({
         start: Date.now() - this.state.time
-      })
-      
+      });
 
-      this.setState({ isOn: true })
-      
-      this.timer = setInterval( () =>
+      this.setState({ isOn: true });
 
-        this.setState({
-          time: Date.now() - this.state.start,
-
-        }), 10
-      )
+      this.timer = setInterval(
+        () =>
+          this.setState({
+            time: Date.now() - this.state.start
+          }),
+        10
+      );
     }
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
   }
 
   stopTimer() {
     this.setState({ isOn: false });
-    clearInterval(this.timer)    
-    
+    clearInterval(this.timer);
   }
-  
+
   splitTime() {
     if (this.state.isOn) {
       this.setState({
-        key: (this.state.key + 1)
-
-      })
+        key: this.state.key + 1
+      });
 
       this.setState(prevState => ({
-        times: [ {lapTime: this.state.time, key: this.state.key }, ...prevState.times]
-      }))
+        times: [
+          { lapTime: this.state.time, key: this.state.key },
+          ...prevState.times
+        ]
+      }));
     }
   }
-  
+
   resetTimer() {
     if (!this.state.isOn) {
-      this.setState({ 
+      this.setState({
         isOn: false,
         time: 0,
         start: 0,
         key: 1,
         times: []
-
       });
     }
   }
 
   render() {
-
-    const lapTimes = this.state.times.map((number) =>
-      <li key={number.key}>{number.key}: {ms(number.lapTime)}</li>
-    );
-
-    const shortEnglishHumanizer = humanizeDuration.humanizer({
-      language: 'shortEn',
+    const textHumanizer = humanizeDuration.humanizer({
+      language: "shortEn",
       languages: {
         shortEn: {
-          h: () => ':',
-          m: () => ':',
-          s: () => '',
-          ms: () => 'ms',
+          h: () => "H",
+          m: () => "M",
+          s: () => "",
+          ms: () => ""
         }
       }
-    })
+    });
+
+    const lapTimes = this.state.times.map(number => (
+      <li key={number.key}>
+        {number.key}: {textHumanizer(number.lapTime)}
+      </li>
+    ));
 
     return (
       <div className="stopwatch">
-        <img id='watch'  src={Watch} alt='stopwatch' />
-        {this.state.time === 0 ? <h1>0</h1>:
-        // <h1> {humanizeDuration(this.state.time), { spacer: '' }} </h1>
-          <h1> {shortEnglishHumanizer(this.state.time, { maxDecimalPoints: 2 })} </h1>
-        }
-
-        <div>
-          {this.state.isOn ? 
-            <img src={Button} alt='stap/start button' className="stopTime" onClick={this.stopTimer} />:
-            <img src={Button} alt='stap/start button' className="startTime" onClick={this.startTimer}  />
-          }
-
-          <img src={Button} alt='lap time button' className="lapTime" onClick={this.splitTime} />
-          <img src={Button} alt='reset button' className="resetTime" onClick={this.resetTimer} />
-
-          <ul>
-            { lapTimes }
-          </ul>
-          
+        <img id="watch" src={Watch} alt="stopwatch" />
+        <div className='watchBackground'>
+          {this.state.time === 0 ? (
+            <h2>0</h2>
+          ) : (
+            <h2> {textHumanizer(this.state.time, { maxDecimalPoints: 2 })} </h2>
+          )}
         </div>
+        <div>
+          {this.state.isOn ? (
+            <img
+              src={Button}
+              alt="stap/start button"
+              className="stopTime"
+              onClick={this.stopTimer}
+            />
+          ) : (
+            <img
+              src={Button}
+              alt="stap/start button"
+              className="startTime"
+              onClick={this.startTimer}
+            />
+          )}
 
+          <img
+            src={Button}
+            alt="lap time button"
+            className="lapTime"
+            onClick={this.splitTime}
+          />
+          <img
+            src={Button}
+            alt="reset button"
+            className="resetTime"
+            onClick={this.resetTimer}
+          />
+
+          <ul>{lapTimes}</ul>
+        </div>
       </div>
     );
   }
