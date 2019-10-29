ae856b8e23160e86966cafd818e8d0e04e3505bd
MR Matthias <matthias.nyman@hotmail.com>
2019-10-29 16:12:53 +0100

    added function thats needed

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -12,58 +12,111 @@ class Todo extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.onChangeEvent = this.onChangeEvent.bind(this);
     this.onRemove = this.onRemove.bind(this);
   }
 
-  handleChange(id) {
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
+  handleChange(e) {
+    if (typeof e === "number") {
+      this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+          if (todo.id === e) {
+            todo.completed = !todo.completed;
+          }
+          return todo;
+        });
+        return {
+          todos: updatedTodos
+        };
       });
-      return {
-        todos: updatedTodos
-      };
+    } else {
+      this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+          if (todo.id === e.id) {
+            todo.edit = !todo.edit;
+          }
+          return todo;
+        });
+        return {
+          todos: updatedTodos
+        };
+      });
+    }
+  }
+
+  handleEdit(input, value) {
+    this.setState({
+      todos: this.state.todos.map(item =>
+        item.id === input.id ? { ...item, text: value } : item
+      )
     });
   }
 
-  onChange(event){
-    this.setState({text: event.target.value});
+  onChangeEvent(value, event) {
+    
+    if (event.key === "Enter") {
+
+    
+
+        // item => item.id === value.id ? console.log(item.text) : console.log('fuck') )
+
+      this.setState({
+        todos: this.state.todos.map(item =>
+          item.id === value.id ? { ...item, edit: false } : item
+        )
+      });
+    }
+  }
+
+
+  onChange(event) {
+    this.setState({ text: event.target.value });
   }
 
   onSubmit = event => {
-    event.preventDefault()
+    event.preventDefault();
     const object = {
       id: Date.now(),
       text: this.state.text,
-      completed: false
+      completed: false,
+      edit: false
     };
 
     this.setState({
-      text: '',
+      text: "",
       todos: [...this.state.todos, object]
     });
   };
 
   onRemove(id) {
-    const filteredArray = this.state.todos.filter(item => item.id !== id)
-    this.setState({todos: filteredArray});
+    const filteredArray = this.state.todos.filter(item => item.id !== id);
+    this.setState({ todos: filteredArray });
   }
 
   render() {
-
     const todoItems = this.state.todos.map(item => (
-      <TodoItems key={item.id} item={item} onRemove={this.onRemove} handleChange={this.handleChange} />
+      <TodoItems
+        key={item.id}
+        item={item}
+        onRemove={this.onRemove}
+        handleChange={this.handleChange}
+        handleEdit={this.handleEdit}
+        onChangeEvent={this.onChangeEvent}
+      />
     ));
 
     return (
       <>
-        <TodoForm text={this.state.text} onSubmit={this.onSubmit} onChange={this.onChange} />
-
-        <div className="todo-list">{todoItems}</div>
+        <TodoForm
+          text={this.state.text}
+          onSubmit={this.onSubmit}
+          onChange={this.onChange}
+        />
+        <div className="todo-list">
+          <div>{todoItems}</div>
+        </div>
       </>
     );
   }
--- src/components/Todo/TodoItems.js
+++ src/components/Todo/TodoItems.js
@@ -7,18 +7,31 @@ function TodoItem(props) {
     textDecoration: "line-through"
   };
 
+
   return (
     <div className="todo-item">
+      <div onClick={() => props.handleChange(props.item.id)}>
+        {props.item.completed ? <>&#10003;</> : <>&#6464;</>}
+      </div>
 
-        <div onClick={ () => props.handleChange(props.item.id) } >
-            {props.item.completed ? <p>&#10003;</p> : <p>&#6464;</p> }
-        </div>
+      {props.item.edit ? (
 
-        <p style={props.item.completed ? completedStyle : null}>
-            {props.item.text}
+        <input value={props.item.text} 
+          onChange={(e) => props.handleEdit(props.item, e.target.value)}  
+          onKeyPress={(key) => props.onChangeEvent(props.item, key)}
+        />
+
+      ) : (
+        <p
+          style={props.item.completed ? completedStyle : null}
+          onClick={(key) => props.handleChange(props.item, key)}
+        >
+          {props.item.text}
         </p>
 
-        <small onClick={() => props.onRemove(props.item.id)}>X</small>
+      )}
+
+      <small onClick={() => props.onRemove(props.item.id)}>X</small>
     </div>
   );
 }
