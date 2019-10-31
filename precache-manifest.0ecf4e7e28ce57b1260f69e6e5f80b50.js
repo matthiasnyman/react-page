2baac3bfa82d2a6fb4ab4ea1e14fa6bd3ab2b7f5
MR Matthias <matthias.nyman@hotmail.com>
2019-10-31 13:49:52 +0100

    Updates

---
+++ precache-manifest.0ecf4e7e28ce57b1260f69e6e5f80b50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4beda2a2c72267f0b31b492db7d49549",
+    "url": "/react-assangment/index.html"
+  },
+  {
+    "revision": "b55f5c569d41419d3426",
+    "url": "/react-assangment/static/css/main.8d2aa289.chunk.css"
+  },
+  {
+    "revision": "1dc5002a4cddb87de7c9",
+    "url": "/react-assangment/static/js/2.4694450d.chunk.js"
+  },
+  {
+    "revision": "b55f5c569d41419d3426",
+    "url": "/react-assangment/static/js/main.f5dc70dc.chunk.js"
+  },
+  {
+    "revision": "5a9b6eb6981f3db1a068",
+    "url": "/react-assangment/static/js/runtime-main.94b801fb.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/react-assangment/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "175e16c5e5f7e4e3d5aab48ea0a7c0f9",
+    "url": "/react-assangment/static/media/stopwatch.175e16c5.png"
+  }
+]);
